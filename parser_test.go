68f1176659dfcd223682d51c963897851eb2b78c
Maryam <mnouh101@gmail.com>
2022-07-21 13:55:14 +0200

    test added

--- parser_test.go
+++ parser_test.go
@@ -121,7 +121,7 @@ line = `
 			t.Errorf("got %#v want\n %#v", got, want)
 		}
 	})
-	t.Run("set value", func(t *testing.T) {
+	t.Run("set value to existent section and key", func(t *testing.T) {
 		
 		parser := NewParser()
 		_ = parser.LoadFromFile("input.ini")
@@ -129,6 +129,22 @@ line = `
 		got := 
 		want := map[string]map[string]string{
 			"owner":    {"name ": " Maryam Nouh", "organization ": " Acme Widgets Inc."},
+			"database": {"server ": " 192.0.2.62", "port ": " 143", "file ": " \"payroll.dat\""},
+		}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %#v want\n %#v", got, want)
+		}
+	})
+	t.Run("set value to nonexistent section and key", func(t *testing.T) {
+		
+		parser := NewParser()
+		_ = parser.LoadFromFile("input.ini")
+		_ = parser.Set("customer", "age", "27")
+		got := 
+		want := map[string]map[string]string{
+			"customer": {"age ":" 27"},
+			"owner":    {"name ": " John Doe", "organization ": " Acme Widgets Inc."},
 			"database": {"server ": " 192.0.2.62", "port ": " 143", "file ": " \"payroll.dat\""},
 		}
 
